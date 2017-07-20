8179ac745bdc6e80ea141b4901286089dfadc37a
DSG1234 <dsgeva1@gmail.com>
2017-07-20 14:46:04 -0400

    Create web.js

---
+++ web.js
@@ -0,0 +1,13 @@
+$("#hackone").click(function() {
+    window.location = ("https://dsg1234.github.io/Dara-Portfolio/hackone.html");
+});
+
+$("#truths").click(function() {
+    window.location = ("https://dsg1234.github.io/Dara-Portfolio/Truths.html");
+});
+$("#jokes").click(function() {
+    window.location = ("https://dsg1234.github.io/Dara-Portfolio/jokestyle.html");
+});
+$("#hacktwo").click(function() {
+    window.location = ("https://dsg1234.github.io/Dara-Portfolio/Beets1.html");
+});
